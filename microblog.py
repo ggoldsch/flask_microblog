fe78045b194ae2468bd2eed898047c836ba89df4
george <george.goldschmied@verizon.net>
2020-01-13 15:36:22 -0500

    end of Ch. 3, DB

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from app_dir_package import app
+from app_dir_package import app, db
+from app_dir_package.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
