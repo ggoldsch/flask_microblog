e572b5fc00dcca30dce6998d653ebcde4a471031
george <george.goldschmied@verizon.net>
2020-01-14 14:02:20 -0500

    end ch 5.

--- app_dir_package/routes.py
+++ app_dir_package/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template, flash, redirect, url_for
+from app_dir_package import db
+from app_dir_package.forms import RegistrationForm
 from app_dir_package import app
 from app_dir_package.forms import LoginForm
+from app_dir_package.models import User
+from flask import render_template, flash, redirect, url_for
+from flask import request
 from flask_login import current_user, login_user, logout_user
 from flask_login import login_required
-from app_dir_package.models import User
-from flask import request
 from werkzeug.urls import url_parse
 
 @app.route('/')
@@ -46,4 +48,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)    
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
 
