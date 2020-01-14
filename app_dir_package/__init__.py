b97b8a88a2ebff4873370757fe8ce98cfdcc151c
george <george.goldschmied@verizon.net>
2020-01-14 12:42:41 -0500

    01/14/20

--- app_dir_package/__init__.py
+++ app_dir_package/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 Migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app_dir_package import routes, models
